b0fbccfc8fcf828422f4fd28a42b3d0aed9effed
Ashraf Sarwar <128461843+ASHRAF-SARWAR@users.noreply.github.com>
2024-09-12 01:16:54 +0500

    Update orderRouter.js

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -7,6 +7,6 @@ const orderRouter = expres.Router();
 orderRouter.post("/addtoorder", auth, order);
 orderRouter.post("/getorders", auth, getOrders);
 orderRouter.post("/updateorder", auth, updateOrderStatus);
-orderRouter.post("/deleteallorders, deleteAll)
+orderRouter.post("/deleteallorders", deleteAll)
 
 export default orderRouter;
